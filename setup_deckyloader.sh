718453f7504e136cbf5541af1e1780d0a5617920
unlemboslukunlem <127316882+unlbslk@users.noreply.github.com>
2025-01-01 15:34:14 +0300

    Update setup_deckyloader.sh

--- setup_deckyloader.sh
+++ setup_deckyloader.sh
@@ -4,3 +4,11 @@ curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/dow
 sudo sed -i 's~TimeoutStopSec=.*$~TimeoutStopSec=2~g' /etc/systemd/system/plugin_loader.service
 sudo systemctl daemon-reload
 sudo systemctl restart plugin_loader.service
+ echo '[Desktop Entry]
+       Name=Uninstall Decky Loader
+       Comment=Uninstalls Decky Loader from your system
+       Exec=sh -c "read -p \"Do you really want to uninstall Decky Loader? (y/n): \" confirm && if [[ \$confirm == \"y\" || \$confirm == \"Y\" ]]; then curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/uninstall.sh | sh; fi"
+       Icon=~/arch-deckify/steamdeck-gaming-return.png
+       Terminal=true
+       Type=Application
+       StartupNotify=false' > "$XDG_DESKTOP_DIR/Uninstall_Decky_Loader.desktop"
