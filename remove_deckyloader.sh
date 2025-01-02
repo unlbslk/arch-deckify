345d26bdcbe28a2db250d9e08bfe116d7b530cc8
unlemboslukunlem <127316882+unlbslk@users.noreply.github.com>
2025-01-02 22:20:20 +0300

    Update remove_deckyloader.sh

--- remove_deckyloader.sh
+++ remove_deckyloader.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-read -p \"Do you really want to uninstall Decky Loader? (y/n): \" confirm && if [[ \$confirm == \"y\" || \$confirm == \"Y\" ]]; then curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/uninstall.sh | sh; fi
+sh -c "read -p \"Do you really want to uninstall Decky Loader? (y/n): \" confirm && if [[ \$confirm == \"y\" || \$confirm == \"Y\" ]]; then curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/uninstall.sh | sh; fi"
