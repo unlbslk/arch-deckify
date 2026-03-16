5e3a4c0bb7de5164667418c45f1a7673418cf39f
unlemboslukunlem <127316882+unlbslk@users.noreply.github.com>
2026-03-16 22:57:16 +0300

    Added Plasma Login Manager support

--- install.sh
+++ install.sh
@@ -5,16 +5,15 @@ if [ "$EUID" -eq 0 ]; then
     exit 1
 fi
 
-echo -e "\e[1;33mWelcome to Arch Deckify Script\e[0m"
+echo -e "\n\n\e[1;33mWelcome to Arch Deckify Script\e[0m"
 echo -e "\e[91mWarning: This script mostly does not work on NVIDIA cards.\e[0m"
-echo -e "\e[37mThis script has been made to work only on SDDM.\e[0m"
-echo -e "\e[37mYou must make additional changes for other display managers.\e[0m"
+echo -e "\e[37mThis script has been made to work only on Plasma Login Manager.\e[0m"
+echo -e "\e[37mYou must make additional changes for other display managers.\n\n\e[0m"
+dm=$(basename "$(readlink /etc/systemd/system/display-manager.service)")
 
-if ! pacman -Qs sddm > /dev/null; then
-    echo "SDDM is not found. (see: https://unlbslk.github.io/arch-deckify/issues/#what-is-the-sddm-and-how-can-i-install-it)"
+if [[ "$dm" != "plasmalogin.service" ]]; then
+    echo -e "\e[31m[ERROR] \e[0mPlasma Login Manager is not found. (see: https://unlbslk.github.io/arch-deckify/issues/#what-is-the-plasma-login-manager-and-how-can-i-install-it)"
     exit 1
-else
-    echo "SDDM is installed."
 fi
 
 sudo whoami
@@ -42,7 +41,7 @@ while true; do
   fi
 done
 
-echo "[1/18] Checking if yay or paru is installed..."
+echo -e "\n\e[36m [1/18]\e[0m Checking if yay or paru is installed...\n"
 
 if command -v yay &> /dev/null; then
     echo "Yay is already installed. (SKIPPED)"
@@ -65,7 +64,7 @@ else
     fi
 fi
 
-echo "[2/18] Checking and enabling multilib repository..."
+echo -e "\n\e[36m [2/18]\e[0m Checking and enabling multilib repository...\n"
 if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
     echo "Enabling multilib repository..."
     echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
@@ -74,10 +73,10 @@ else
     echo "Multilib repository is already enabled. (SKIPPED)"
 fi
 
-echo "[3/18] Updating the system..."
+echo -e "\n\e[36m [3/18]\e[0m Updating the system...\n"
 sudo pacman -Syu --noconfirm
 
-echo "[4/18] Checking if Steam is installed..."
+echo -e "\n\e[36m [4/18]\e[0m Checking if Steam is installed...\n"
 if ! command -v steam &> /dev/null; then
     echo "Steam is not installed. Installing Steam..."
     sudo pacman -S steam --noconfirm
@@ -85,36 +84,26 @@ else
     echo "Steam is already installed. (SKIPPED)"
 fi
 
-echo "[5/18] Installing gamescope-session-steam-git from AUR..."
+echo -e "\n\e[36m [5/18]\e[0m Installing gamescope-session-steam-git from AUR...\n"
 yay -S --aur gamescope-session-steam-git --noconfirm --sudoloop || paru -S --aur gamescope-session-steam-git --noconfirm
 
-CONFIG_FILE="/etc/sddm.conf"
-echo "[6/18] Configuring auto-login for SDDM..."
-sudo tee /etc/sddm.conf > /dev/null <<EOF
+
+CONFIG_FILE="/etc/plasmalogin.conf"
+echo "[6/18] Configuring auto-login for Plasma Login Manager..."
+sudo tee /etc/plasmalogin.conf > /dev/null <<EOF
 [Autologin]
 Relogin=true
 Session=$selected_de
 User=$(whoami)
-
-[General]
-HaltCommand=/usr/bin/systemctl poweroff
-RebootCommand=/usr/bin/systemctl reboot
-
-[Theme]
-Current=
-
-[Users]
-MaximumUid=60513
-MinimumUid=1000
 EOF
 
 echo "Autologin configured for user: $(whoami)"
 
-echo "[7/18] Creating /usr/bin/steamos-session-select"
+echo -e "\n\e[36m [7/18]\e[0m Creating /usr/bin/steamos-session-select\n"
 
 echo '#!/usr/bin/bash
 
-CONFIG_FILE="/etc/sddm.conf"
+CONFIG_FILE="/etc/plasmalogin.conf"
 
 # If no arguments are provided, list valid arguments
 if [ $# -eq 0 ]; then
@@ -131,7 +120,7 @@ if [ "$1" == "plasma" ] || [ "$1" == "desktop" ]; then
     
     echo "Switching session to Desktop."
     if [ ! -f "$CONFIG_FILE" ]; then
-        echo "SDDM config file could not be found at $CONFIG_FILE."
+        echo "Plasma Login Manager config file could not be found at $CONFIG_FILE."
         exit 1
     fi
     NEW_SESSION='$selected_de' # For other desktops, change here.
@@ -143,7 +132,7 @@ elif [ "$1" == "gamescope" ]; then
     
     echo "Switching session to Gamescope."
     if [ ! -f "$CONFIG_FILE" ]; then
-        echo "SDDM config file could not be found at $CONFIG_FILE."
+        echo "Plasma Login Manager config file could not be found at $CONFIG_FILE."
         exit 1
     fi
     NEW_SESSION="gamescope-session-steam"
@@ -154,28 +143,28 @@ else
     exit 1
 fi' | sudo tee /usr/bin/steamos-session-select > /dev/null
 
-echo "[8/18] Making /usr/bin/steamos-session-select executable..."
+echo -e "\n\e[36m [8/18]\e[0m Making /usr/bin/steamos-session-select executable...\n"
 sudo chmod +x /usr/bin/steamos-session-select
 
-echo "[9/18] Making SDDM session config editable without prompting sudo password..."
+echo -e "\n\e[36m [9/18]\e[0m Creating Plasma Login Manager sudoers rule...\n"
 sudoers_file="/etc/sudoers.d/sddm_config_edit"
 if [ ! -f "$sudoers_file" ]; then
     echo "ALL ALL=(ALL) NOPASSWD: /usr/bin/sed -i s/^Session=*/Session=*/ ${CONFIG_FILE}" | sudo tee "$sudoers_file" > /dev/null
     sudo chmod 440 "$sudoers_file"
 else
-    echo "Passwordless sudo for editing SDDM session config is already set. (SKIPPED)"
+    echo "Sudoers rule already set. (SKIPPED)"
 fi
 
 
-echo "[10/18] Installing MangoHUD"
+echo -e "\n\e[36m [10/18]\e[0m Installing MangoHUD\n"
 if ! pacman -Qs mangohud > /dev/null; then
     
     sudo pacman -S mangohud --noconfirm
 else
-    echo "MangoHUD is already installed (SKIPPED)."
+    echo "MangoHUD is already installed. (SKIPPED)"
 fi
 
-echo "[11/18] Installing wget"
+echo -e "\n\e[36m [11/18]\e[0m Installing wget\n"
 if ! pacman -Qs wget > /dev/null; then
     
     sudo pacman -S wget --noconfirm
@@ -183,7 +172,7 @@ else
     echo "wget is already installed (SKIPPED)."
 fi
 
-echo "[12/18] Installing ntfs-3g (required for NTFS drives)"
+echo -e "\n\e[36m [12/18]\e[0m Installing ntfs-3g (for NTFS partitions)\n"
 if ! pacman -Qs ntfs-3g > /dev/null; then
     
     sudo pacman -S ntfs-3g --noconfirm
@@ -191,7 +180,7 @@ else
     echo "ntfs-3g is already installed (SKIPPED)."
 fi
 
-echo "[13/18] Installing Gamescope"
+echo -e "\n\e[36m [13/18]\e[0m Installing Gamescope\n"
 if ! pacman -Qs gamescope > /dev/null; then
     
     sudo pacman -S gamescope --noconfirm
@@ -199,12 +188,12 @@ else
     echo "Gamescope is already installed (SKIPPED)."
 fi
 
-echo "[14/18] Making the brightness slider work..."
+echo -e "\n\e[36m [14/18]\e[0m Creating backlight rule for handheld devices\n"
 sudo usermod -a -G video $(whoami)
 if ! grep -q 'ACTION=="add", SUBSYSTEM=="backlight"' /etc/udev/rules.d/backlight.rules; then
     echo 'ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"' | sudo tee -a /etc/udev/rules.d/backlight.rules
 fi
-echo "[15/18] Downloading Gaming Mode shortcut icon..."
+echo -e "\n\e[36m [15/18]\e[0m Creating Gaming Mode shortcut icon...\n"
 
 mkdir ~/arch-deckify
 if [ ! -f ~/arch-deckify/steam-gaming-return.png ]; then
@@ -269,12 +258,12 @@ if [ ! -e "/usr/share/applications/Deckify_Tools.desktop" ]; then
     rm -rf "$(xdg-user-dir)/Deckify_Tools.desktop"
 fi
 
-echo "[16/18] 'Return to Gaming Mode' and 'Helper 'has been added to desktop and application menu."
+echo -e "\n\e[36m [16/18]\e[0m 'Return to Gaming Mode' and 'Helper' shortcut has been added to desktop and application menu.\n"
 
 sudo pacman -S bluez bluez-utils --noconfirm
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
-echo "[17/18] Bluetooth service enabled and started."
+echo -e "\n\e[36m [17/18]\e[0m Bluetooth service enabled and started.\n"
 
 
 update_script_path="$HOME/arch-deckify/system_update.sh"
@@ -283,7 +272,7 @@ cat <<EOL > "$update_script_path"
 AUR_HELPER=""; UPDATE_CMD=""; command -v yay &>/dev/null && AUR_HELPER="yay" && UPDATE_CMD="yay -Syu --sudoloop --noconfirm" || { command -v paru &>/dev/null && AUR_HELPER="paru" && UPDATE_CMD="paru -Syu --noconfirm"; }; [ -z "\$AUR_HELPER" ] && echo -e "\e[91mError: Neither yay nor paru is installed.\e[0m" && sleep 10 && exit 1; (konsole -e bash -c "clear; echo -e '\n\n\e[94mEnter your sudo password:\nYou can open keyboard by pressing GUIDE+X or PS+SQUARE on controller.\n\n\e[0m'; sudo rm -rf /var/lib/pacman/db.lck; \$UPDATE_CMD; echo -e '\n\e[96mSystem packages have been updated.\e[0m'; if flatpak --version &>/dev/null; then echo -e '\n\e[96mUpdating Flathub...\e[0m'; flatpak update -y; echo -e '\e[93mFlatpak updated.\e[0m'; else echo 'Skipped Flatpak.'; fi; echo -e '\e[93mFinished. This window will be closed in 5 seconds...\e[0m'; sleep 5; exit") || (gnome-terminal -- bash -c "clear; echo -e '\n\n\e[94mEnter your sudo password:\nYou can open keyboard by pressing GUIDE+X or PS+SQUARE on controller.\n\n\e[0m'; sudo rm -rf /var/lib/pacman/db.lck; \$UPDATE_CMD; echo -e '\e[96mSystem packages have been updated.\e[0m'; if flatpak --version &>/dev/null; then echo -e '\n\e[96mUpdating Flathub...\e[0m'; flatpak update -y; echo -e '\e[93mFlatpak updated.\e[0m'; else echo 'Skipped Flatpak.'; fi; echo -e '\n\e[93mExecuted. This window will be closed in 5 seconds...\e[0m\n'; sleep 5; exit") || (kgx -- bash -c "clear; echo -e '\n\n\e[94mEnter your sudo password:\nYou can open keyboard by pressing GUIDE+X or PS+SQUARE on controller.\n\n\e[0m'; sudo rm -rf /var/lib/pacman/db.lck; \$UPDATE_CMD; echo -e '\e[96mSystem packages have been updated.\e[0m'; if flatpak --version &>/dev/null; then echo -e '\n\e[96mUpdating Flathub...\e[0m'; flatpak update -y; echo -e '\e[93mFlatpak updated.\e[0m'; else echo 'Skipped Flatpak.'; fi; echo -e '\n\e[93mExecuted. This window will be closed in 5 seconds...\e[0m\n'; sleep 5; pkill kgx") || (kitty bash -c "clear; echo -e '\n\n\e[94mEnter your sudo password:\nYou can open keyboard by pressing GUIDE+X or PS+SQUARE on controller.\n\n\e[0m'; sudo rm -rf /var/lib/pacman/db.lck; \$UPDATE_CMD; echo -e '\e[96mSystem packages have been updated.\e[0m'; if flatpak --version &>/dev/null; then echo -e '\n\e[96mUpdating Flathub...\e[0m'; flatpak update -y; echo -e '\e[93mFlatpak updated.\e[0m'; else echo 'Skipped Flatpak.'; fi; echo -e '\n\e[93mExecuted. This window will be closed in 5 seconds...\e[0m\n'; sleep 5; exit") || (alacritty -e bash -c "clear; echo -e '\n\n\e[94mEnter your sudo password:\nYou can open keyboard by pressing GUIDE+X or PS+SQUARE on controller.\n\n\e[0m'; sudo rm -rf /var/lib/pacman/db.lck; \$UPDATE_CMD; echo -e '\e[96mSystem packages have been updated.\e[0m'; if flatpak --version &>/dev/null; then echo -e '\n\e[96mUpdating Flathub...\e[0m'; flatpak update -y; echo -e '\e[93mFlatpak updated.\e[0m'; else echo 'Skipped Flatpak.'; fi; echo -e '\n\e[93mExecuted. This window will be closed in 5 seconds...\e[0m\n'; sleep 5; exit")
 EOL
 chmod +x "$update_script_path"
-echo "[18/18] 'system_update.sh' has been added to $update_script_path"
+echo -e "\n\e[36m [18/18]\e[0m 'system_update.sh' has been added to $update_script_path\n"
 echo -e "\n\n\e[1;33mInstallation is complete. We recommend you to reboot your system.\e[0m"
-echo -e "\n\e[1;33mYou can try by clicking Gaming Mode shortcut.\e[0m"
+echo -e "\n\e[1;30mIf you encounter an issue (such as being stuck on a black screen), check here:\e[0m https://unlbslk.github.io/arch-deckify/issues/"
 echo -e "\n\e[37mYou can update the system in Steam by adding the ~/arch-deckify/system_update.sh file to Steam as a non-Steam game while in desktop mode.\nUnfortunately, system updates are not possible through Steam settings.\e[0m\n\n"
